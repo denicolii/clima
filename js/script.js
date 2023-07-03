91de52ee36cb316496e97228357b67e0d1bf89d5
denicolii <110945232+denicolii@users.noreply.github.com>
2023-07-03 15:05:20 -0300

    js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,6 @@
 const apikey = "8473e85c4d6aa9a5be834ad29d7640b9" ;
 const apiCountryURL = "https://countryflagsapi.com/png/";
 const apiUnsplash = "https://source.unsplash.com/1600x900/?";
+
+const cityInput = document.querySelector("#city-input");
+const serachBtn = document.querySelector("#search");
