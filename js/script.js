0c4bffc5a478069c0c70cb2d01ddd8db07f0eb6f
denicolii <110945232+denicolii@users.noreply.github.com>
2023-07-03 14:25:16 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// 8473e85c4d6aa9a5be834ad29d7640b9 KEY
+
+const apikey = "8473e85c4d6aa9a5be834ad29d7640b9" ;
+const apiCountryURL = "https://countryflagsapi.com/png/";
+const apiUnsplash = "https://source.unsplash.com/1600x900/?";
